0f4cd6d071427f682e714801f854f1bb80570a02
secadmin <wangxichang@126.com>
2022-05-14 08:39:00 +0800

    add readline to ch9.demo_open_newline

--- chapter09/open_newline_usage.py
+++ chapter09/open_newline_usage.py
@@ -6,9 +6,10 @@ demo the usage of parameter newline in open function
 import os
 
 demo_text = "Line1\nLine2\rLine3\r\n"
+demo_file = "temp_newline.txt"
 
 
-def demo_open_write_with_newline(filename="demo_newline.txt", newline=None):
+def demo_open_write_with_newline(filename=demo_file, newline=None):
     """
     展示写文本文件时newline的作用
     将newline分别设置为None, "\n", "", "\r"时，文本写入文件的情况
@@ -16,19 +17,26 @@ def demo_open_write_with_newline(filename="demo_newline.txt", newline=None):
     if newline is set to None, "\n", "", "\r", etc
 
     test result:
-    newline=None，将文本中的“\r”转换为os.linesep，
-    newline="\n" or "", 不做任何替换
-    newline=any_legal_char, 定义：将\n替换为any_legal_char，
+    newline=None，使用universal Newline模式（PEP278），文本中的“\n”被视为换行符，写入时将其转换为os.linesep；
+    newline="\n", 写入时对文本内容不做任何替换；
+    newline="", 写入时对文本内容不做任何替换；
+    newline="\r", 写入时将"\n"转换为“\r”；
+    newline="\r\n", 写入时将"\n"转换为“\r\n”；
+
+    etc:
+    newline=any_legal_char, 定义：将\n替换为any_legal_char；
         测试：对合法字符的定义不明确，各种其它字符对写数据没有进行替换
     """
     print("\n\nwrite text to file\n"+'-'*100)
     newline_str = str(newline.encode('utf8')) if isinstance(newline, str) else "None"
     print(f"newline={newline_str}")
+    print("raw text:", demo_text.encode('utf8'))
 
+    # write demo_text to file with newline
     with open(filename, "w+", newline=newline, encoding="utf8") as fp:
         fp.write(demo_text)
 
-    print("raw text:", demo_text.encode('utf8'))
+    # read from file to compare with raw_text
     with open(filename, "rb") as fp:
         readbytes = fp.read()
         if newline is None:
@@ -38,22 +46,21 @@ def demo_open_write_with_newline(filename="demo_newline.txt", newline=None):
         elif newline == "\r":
             print("write to:", readbytes, "\t# " + r" \n ==> \r")
         else:
-            print("write to:", readbytes, "\t# "+r" \n ==> "+str(newline.encode('utf8'))+"?")
+            print("write to:", readbytes, "\t# "+r" \n ==> "+str(newline.encode('utf8')))
 
 
-def demo_open_read_with_newline(filename="demo_newline.txt", newline=None):
+def demo_open_read_with_newline(filename=demo_file, newline=None):
     """
     展示读文本文件时，设置newline的效果
     分别设置为None, “”， "\n", "\r"时，文本文件读出数据的情况
-    demo what happens when reading text from text file
-    if newline is set to None, "\n", "", "\r", etc
+    demo what happens when reading text from text file if newline is set to None, "\n", "", "\r", "\r\n"
 
     test result:
-    newline=None，将文本中的'\r', '\n' '\r\n'都视为行结束符，都转换为'\n'
-    newline='',
-    newline='\r',
-    newline='\n',
-    newline=any_legal_char,
+    newline=None，将文本中的'\r', '\n' '\r\n'都视为行结束符，读出后都转换为'\n';
+    newline='', 将文本中的'\r', '\n' '\r\n'都视为行结束符，读出内容不做转换;
+    newline='\r', 将文本中的'\r'视为行结束符，读出内容不做转换;
+    newline='\n', 将文本中的'\n'视为行结束符，读出内容不做转换;
+    newline='\r\n', 将文本中的'\r\n'视为行结束符，读出内容不做转换;
     """
 
     with open(filename, "w+", newline="", encoding="utf8") as fp:
@@ -69,20 +76,31 @@ def demo_open_read_with_newline(filename="demo_newline.txt", newline=None):
         # read by read
         readbytes = fp.read().encode('utf8')
         if newline is None:
-            print("read  to:", readbytes, "\t# "+r"'\r' or '\n' ==> '\n'")
+            print("read  to:", readbytes, "\t# no replace")
         elif newline == "":
-            print("read  to:", readbytes, "\t# "+r"\n ==> \r\n")
+            print("read  to:", readbytes, "\t# no replace")
         elif newline == "\r":
-            print("read  to:", readbytes, "\t# "+r"\n ==> \r\n")
+            print("read  to:", readbytes, "\t# no replace")
         elif newline == "\n":
-            print("read  to:", readbytes, "\t# "+r"\n ==> \r\n")
+            print("read  to:", readbytes, "\t# no replace")
         else:
-            print("read  to:", readbytes, "\t# "+r"\n ==> \r\n")
+            print("read  to:", readbytes, "\t# no replace")
+        print("-."*40)
         # read by readlines
         print(f"# use {newline_str} as line-end-char to get each line")
+        print("--- readlines ---")
         fp.seek(0)
         for lno, line in enumerate(fp.readlines()):
-            print(f"line-{lno+1}:{line.encode('utf8')}")
+            print(f"(line-{lno+1}):{line.encode('utf8')}")
+        print("--- readline ---")
+        fp.seek(0)
+        line_no = 1
+        while True:
+            line = fp.readline()
+            if line == "":
+                break
+            print(f"(line-{line_no}):{line.encode()}")
+            line_no += 1
 
 
 if __name__ == "__main__":
@@ -93,10 +111,10 @@ if __name__ == "__main__":
     demo_open_write_with_newline(newline='\n')
     demo_open_write_with_newline(newline='\r')
     demo_open_write_with_newline(newline='\r\n')
+    # demo_open_write_with_newline(newline='\n\r')  # invalid newline char
     # demo read
     demo_open_read_with_newline(newline=None)
     demo_open_read_with_newline(newline="")
     demo_open_read_with_newline(newline="\r")
     demo_open_read_with_newline(newline="\n")
-    # demo_open_read_with_newline(newline="\b")
     demo_open_read_with_newline(newline="\r\n")
