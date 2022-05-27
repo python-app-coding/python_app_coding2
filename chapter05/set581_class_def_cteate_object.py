2a45b7e5f56eef7a8671e1e705a033c75bb8798f
secadmin <wangxichang@126.com>
2022-05-27 22:30:06 +0800

    new sec581

--- chapter05/set581_class_def_cteate_object.py
+++ chapter05/set581_class_def_cteate_object.py
@@ -13,3 +13,17 @@ class Foo:
 
 foo = Foo(100)
 print("foo.x = ", foo.x)
+
+
+class Foo2:
+    def __new__(cls, *args, **kwargs):  # 重新对象创建方法__new__
+        print("object is created!")
+        return None			            # 返回创建的对象
+
+    def __init__(self, x):		        # 定义对象初始化方法
+        print("Object is initialized!")  # 新增创建过程代码
+        self.x = x			            # 设置对象属性
+
+
+foo2 = Foo2(100)
+print(type(foo2))
