0b063eddfeb1a89c3bd63fe94cceba65ad453f09
admin <wangxichang@126.com>
2022-06-07 18:14:28 +0800

    new sec832

--- chapter08/sec83/sec831_file_cmp.py
+++ chapter08/sec83/sec831_file_cmp.py
@@ -1,38 +1,70 @@
 # coding = utf8
+
+import filecmp
 import difflib
+import shutil
+import os
 
 
-with open("cmpfile01.txt", 'wt') as fp01, open("cmpfile02.txt", 'wt') as fp02:
-     fp01.writelines(
-         "this is line01\n"
-         "this is line02\n"
-         "this is line03\n")
-     fp02.writelines(
-         "this is line0102\n"
-         "this is line02\n"
-         "this is line0203\n"
-         "this is line0204\n")
-
-with open('cmpfile01.txt') as fp1, open('cmpfile02.txt') as fp2:
-    lines1 = fp1.readlines()
-    lines2 = fp2.readlines()
-    for line in difflib.Differ().compare(lines1, lines2):
-        print(line, end='')
-# 比较结果(-: 第一文件的文本， +: 第二文件的文本， ？: 标识差异位置， ' ':两个文本行相同的内容)
-# - this is line01
-# + this is line0102
-# ?               ++	# 在该位置出现不同
-#   this is line02	# 该行都相同
-# - this is line03
-# + this is line0203
-# ?              ++	# 在该位置出现不同
-# + this is line0204	# 该行只在第二个文件存在
+def exp_filecmp():
+    if not os.path.isdir('work'):
+        os.mkdir('work')
+        with open("work/cmpfile1.txt", 'wt') as fp1, \
+             open("work/cmpfile2.txt", 'wt') as fp2, \
+             open("work/cmpfile3.txt", 'wt') as fp3:
+            fp1.write("Hello-World\nHello-China")
+            fp2.write("Hello World\nHello-China")
+            fp3.write("Hello-World\nHello-China")
+    print(filecmp.cmp("work/cmpfile1.txt", "work/cmpfile2.txt"))
+    # True
+    print(filecmp.cmp("work/cmpfile1.txt", "work/cmpfile2.txt", shallow=False))
+    # False
+    print(filecmp.cmp("work/cmpfile1.txt", "work/cmpfile3.txt"))
+    # True
+    print(filecmp.cmp("work/cmpfile1.txt", "work/cmpfile3.txt", shallow=False))
+    # True
 
 
-s = difflib.SequenceMatcher(a="abxcd", b="abcd")	# 比较两个字符串（字符序列）
-cr = s.get_matching_blocks()	    # 比较结果是元素为命名元组的列表
-for m in cr:
-    print(m)
-# Match(a=0, b=0, size=2),			# 0位置开始2个字符相同,即a[0:2] == b[0:2]
-# Match(a=3, b=2, size=2), 			# a=3,b=2位置，2个字符相同,a[3:5] == b[2:4]
-# Match(a=5, b=4, size=0)			# 最后一个是无效结果（dummy）
+
+
+
+def exp_difflib():
+    with open("cmpfile01.txt", 'wt') as fp01, open("cmpfile02.txt", 'wt') as fp02:
+         fp01.writelines(
+             "this is line01\n"
+             "this is line02\n"
+             "this is line03\n")
+         fp02.writelines(
+             "this is line0102\n"
+             "this is line02\n"
+             "this is line0203\n"
+             "this is line0204\n")
+
+    with open('cmpfile01.txt') as fp1, open('cmpfile02.txt') as fp2:
+        lines1 = fp1.readlines()
+        lines2 = fp2.readlines()
+        for line in difflib.Differ().compare(lines1, lines2):
+            print(line, end='')
+    # 比较结果(-: 第一文件的文本， +: 第二文件的文本， ？: 标识差异位置， ' ':两个文本行相同的内容)
+    # - this is line01
+    # + this is line0102
+    # ?               ++	# 在该位置出现不同
+    #   this is line02	# 该行都相同
+    # - this is line03
+    # + this is line0203
+    # ?              ++	# 在该位置出现不同
+    # + this is line0204	# 该行只在第二个文件存在
+
+
+    s = difflib.SequenceMatcher(a="abxcd", b="abcd")	# 比较两个字符串（字符序列）
+    cr = s.get_matching_blocks()	    # 比较结果是元素为命名元组的列表
+    for m in cr:
+        print(m)
+    # Match(a=0, b=0, size=2),			# 0位置开始2个字符相同,即a[0:2] == b[0:2]
+    # Match(a=3, b=2, size=2), 			# a=3,b=2位置，2个字符相同,a[3:5] == b[2:4]
+    # Match(a=5, b=4, size=0)			# 最后一个是无效结果（dummy）
+
+
+if __name__ == '__main__':
+    exp_filecmp()
+    # exp_difflib()
---
+++ chapter08/sec83/sec832_dir_cmp.py
@@ -0,0 +1,101 @@
+# coding = utf8
+
+import os
+import filecmp
+from filecmp import dircmp
+
+
+def exp_filecmp_cmpfiles():
+    if not os.path.isdir('work'):
+        os.mkdir('work')
+        with open("work/cmpfile1.txt", 'wt') as fp1, \
+                open("work/cmpfile2.txt", 'wt') as fp2, \
+                open("work/cmpfile3.txt", 'wt') as fp3:
+            fp1.write("Hello-World\nHello-China")
+            fp2.write("Hello World\nHello-China")
+            fp3.write("Hello-World\nHello-China")
+    if not os.path.isdir('work2'):
+        os.mkdir('work2')
+        with open("work2/cmpfile1.txt", 'wt') as fp1, \
+                open("work2/cmpfile2.txt", 'wt') as fp2:
+            fp1.write("Hello\nHello-China")
+            fp2.write("Hello World\nHello-China")
+    print("shallow cmp: work-work2")
+    print(filecmp.cmpfiles("work", "work2",
+                           common=["cmpfile1.txt", "cmpfile2.txt", "cmpfile3.txt"],
+                           shallow=True
+                           ))
+    print("deep cmp: work-work2")
+    print(filecmp.cmpfiles("work", "work2",
+                           common=["cmpfile1.txt", "cmpfile2.txt", "cmpfile3.txt"],
+                           shallow=False
+                           ))
+
+
+
+def exp_filecmp_dircmp():
+    print(filecmp.dircmp('work', 'work2').report())
+    # diff work work2			# 差异比较目录
+    # Only in work : ['cmpfile3.txt']	# 仅在一个目录的文件
+    # Identical files : ['cmpfile2.txt']	# 相同的文件
+    # Differing files : ['cmpfile1.txt']	# 不相同的文件
+
+
+
+def exp_filecmp_dircmp2():
+    """
+    ●─ worka [path] [files:0 dirs:3]
+      └─◎─work1 [path] [files:2 dirs:0]
+      │  ├─cmpfile1.txt		# 内容：Wello-World\nHello-China
+      │  └─cmpfile2.txt		# 内容：Wello World\nHello-China
+      └─◎─work2 [path] [files:1 dirs:0]
+      │  └─cmpfile1.txt		# 内容：Wello-World\nHello-China
+      └─◎─work3 [path] [files:1 dirs:0]
+          └─cmpfile3.txt		# 内容：Wello-World\nHello-China
+    ●─◎─workb[path] [files:0 dirs:4]
+      └─◎─work1 [path] [files:1 dirs:0]
+      │  └─cmpfile1.txt		# 内容：Wello-World\nHello-China
+      └─◎─work2 [path] [files:2 dirs:0]
+      │  ├─cmpfile1.txt		# 内容：Wello-World\nHello-China
+      │  └─cmpfile2.txt		# 内容：Wello World\nHello-China
+      └─◎─work3 [path] [files:1 dirs:0]
+      │  └─cmpfile3.txt		# 内容：Wello-World\nHello-China
+      └─◎─work4 [path] [files:0 dirs:0]
+    """
+    if not os.path.isdir('worka'):
+        os.mkdir('worka')
+        with open("worka/cmpfile1.txt", 'wt') as fp1, \
+                open("worka/cmpfile2.txt", 'wt') as fp2, \
+                open("worka/cmpfile3.txt", 'wt') as fp3:
+            fp1.write("Hello-World\nHello-China")
+            fp2.write("Hello World\nHello-China")
+            fp3.write("Hello-World\nHello-China")
+    if not os.path.isdir('work2'):
+        os.mkdir('workb')
+        with open("workb/cmpfile1.txt", 'wt') as fp1, \
+                open("workb/cmpfile2.txt", 'wt') as fp2:
+            fp1.write("Hello\nHello-China")
+            fp2.write("Hello World\nHello-China")
+    print("dircmp report_full_closure:")
+    print(filecmp.dircmp('worka', 'workb').report_full_closure())
+
+    # 有关比较结果的属性使用惰性计算方式，由__getattr__调用获取
+    cmp = filecmp.dircmp('worka', 'workb')
+    print(cmp.__getattr__('same_files'))		# 获取相同文件情况（使用__getattr__访问属性）
+    # []
+    print(cmp.diff_files)			# 获取不同文件情况（直接使用.方式）
+    # []					# 仅提供当前一层目录的比较情况
+    print(cmp.subdirs)			# 提供子目录比较结果
+    # {'work1': <filecmp.dircmp object at 0x0000024E18C6E438>,
+    #  'work2': <filecmp.dircmp object at 0x0000024E18C6ECC0>,
+    #  'work3': <filecmp.dircmp object at 0x0000024E18C6E898>}
+    
+    print(cmp.subdirs['work2'].report())		    # 报告具体子目录比较结果
+    # diff worka\work2 workb\work2
+    # Only in workb\work2 : ['cmpfile2.txt']  		# 文件cmpfile2.txt仅在workb\work2中存在
+    # Identical files : ['cmpfile1.txt']      		# 文件cmpfile1.txt在两个目录中存在且相同
+
+
+if __name__ == '__main__':
+    # exp_filecmp_cmpfiles()
+    exp_filecmp_dircmp()
