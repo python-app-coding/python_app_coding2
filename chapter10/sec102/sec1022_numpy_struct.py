426012408f271d8a0fb2e38c2b2462fbb0c088b2
admin <wangxichang@126.com>
2022-06-09 12:09:30 +0800

    new sec102

--- chapter10/sec102/sec1022_numpy_struct.py
+++ chapter10/sec102/sec1022_numpy_struct.py
@@ -3,17 +3,86 @@
 import numpy as np
 
 
-def demo1():
+def demo0_dtype_char():
     """
-    >>> persons = [('Wang', 18, 120), ('Li', 20, 150)]		# Python数据：姓名、年龄、身高
-    >>> pdata = np.array(persons, dtype='U10, i2, int16')		# 使用复合类型定义一个数组
+    # 使用dtype(typestr)可以查看各个类型的Numpy对应名称
+    >>> np.dtype(np.float_)
+    dtype('float64')
+
+    >>> [np.dtype(t) for t in (np.float16, np.str_, np.int32)]
+    [dtype('float16'), dtype('<U'), dtype('int32')]
+    >>> [np.dtype(t).char for t in (np.float16, np.str_, np.int32)]
+    ['e', 'U', 'l']
+
+    >>> type21 = [np.bool, np.int_, np.intc, np.intp, np.int8, np.int16, np.int32, np.int64,
+    ...           np.uint8, np.uint16, np.uint32, np.uint64,
+    ...           np.float, np.float16, np.float32, np.float64,
+    ...           np.complex_, np.complex64, np.complex128,
+    ...           np.str_, np.string_]
+    >>> [np.dtype(t).char for t in type21]
+    ['?', 'l', 'i', 'q', 'b', 'h', 'l', 'q', 'B', 'H', 'L', 'Q', 'd', 'e', 'f', 'd', 'D', 'F', 'D', 'U', 'S']
+    >>> for t in type21:
+    ...     print(f'{np.dtype(t).char}: {np.dtype(t).name}')
+
+    # 使用dtype.char属性，可以查看各种类型的单字母名称
+    >>> ctype = "?, B, bool, D, e, F"
+    >>> a = np.array([(1, 2, 0, 3+2j, 3, 2j)], dtype=ctype)
+
+    >>> [a['f'+str(i)][0].dtype for i in range(6)]
+    [dtype('bool'), dtype('uint8'), dtype('bool'), dtype('complex128')]
+
+    >>> [a['f'+str(i)][0].dtype.char for i in range(6)]
+    ['?', 'B', '?', 'D']
+    """
+
+
+
+def demo1_tuple_mode():
+    """
+    # 源数据有4个字段的数据：姓名、年龄、身高、体重
+    >>> persons = [('Wang', 18, 156, 60), ('Li', 20, 177, 72)]
+
+    # 定义具有4个字段的复合结构类型：长度为10
+    >>> ctype1 = np.dtype([('name', 'U10'),('age', 'i2'), ('height', np.float_),('weight', 'float16')])
+
+    # 使用复合类型创建多维数组
+    >>> pdata = np.array(persons, dtype=ctype1)
     >>> pdata
-    array([('Wang', 18, 120), ('Li', 20, 150)],
-          dtype=[('f0', '<U10'), ('f1', '<i2'), ('f2', '<i2')])
+    array([('Wang', 18, 156., 60.), ('Li', 20, 177., 72.)],
+          dtype=[('name', '<U10'), ('age', '<i2'), ('height', '<f8'), ('weight', '<f2')])
     """
 
 
-def demo2():
+
+def demo2_str_mode():
+    """
+    # 源数据有4个字段的数据：姓名、年龄、身高、体重
+    >>> persons = [('Wang', 18, 156, 60), ('Li', 20, 177, 72)]
+
+    >>> pdata = np.array(persons, dtype='U10, i2, float, float16')		# 使用复合类型定义一个数组
+    >>> pdata
+    array([('Wang', 18, 156., 60.), ('Li', 20, 177., 72.)],
+          dtype=[('f0', '<U10'), ('f1', '<i2'), ('f2', '<f4'), ('f3', '<f2')])
+    """
+
+
+def demo3_dict_mode():
+    """
+    # 源数据有4个字段的数据：姓名、年龄、身高、体重
+    >>> persons = [('Wang', 18, 156, 60), ('Li', 20, 177, 72)]
+
+    >>> ctype_dict = {'names': ['name', 'age', 'height', 'weight'],
+    ...               'formats': ['U10', 'i2', np.float64, np.float16]
+    ...               }
+
+    >>> pdata = np.array(persons, dtype=ctype_dict)
+    >>> pdata
+    array([('Wang', 18, 156., 60.), ('Li', 20, 177., 72.)],
+          dtype=[('name', '<U10'), ('age', '<i2'), ('height', '<f8'), ('weight', '<f2')])
+    """
+
+
+def demo_nest():
     """
     >>> persons = [('Wang', [18, 120]), ('Li', [20, 150])]	# Python数据
 
@@ -31,7 +100,7 @@ def demo2():
     (2,)
     """
 
-def demo3():
+def demo4():
     """
     # 学生成绩数据
     >>> data = [
