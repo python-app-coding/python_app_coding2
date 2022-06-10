935257970fe81290130f0fc1a36a99203c72619c
wangxichang <wangxichang@126.com>
2022-06-10 12:34:50 +0800

    renew sec1123

--- chapter11/sec112/sec1123_write_read_csv.py
+++ chapter11/sec112/sec1123_write_read_csv.py
@@ -32,11 +32,26 @@ def demo1_to_csv():
 
 def demo2_read_csv():
     """
+    >>> dfread = pd.read_csv('temp_csv_file_1.csv')
 
+    # 由于没有指定index,原索引标签被作为第一列读出
+    # 在标题行中没有第一列的名称，赋予一个临时名称
+    >>> dfread
+       Unnamed: 0         name  yw  sx  wy   zf
+    0         101  Zhai Linwei  80  87  92  259
+    1         102  Zhou Geshan  90  80  88  258
+    2         103  Zang Keting  67  73  70  210
     """
 
 
 def demo3_column_type():
     """
+    >>> dfscore = pd.read_csv('student_score.csv'，index_col=0)
 
+	# 将第一列读出为索引标签，但识别为整数类型
+    >>> dfscore
+                name  yw  sx  wy   zf
+    101  Zhai Linwei  80  87  92  259
+    102  Zhou Geshan  90  80  88  258
+    103  Zang Keting  67  73  70  210
     """
