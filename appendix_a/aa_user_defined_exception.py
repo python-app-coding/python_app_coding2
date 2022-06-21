e586f62ab3a1f88211312ec12dcefdd00ecc6e0f
admin <wangxichang@126.com>
2022-06-21 16:19:22 +0800

    renew

---
+++ appendix_a/aa_user_defined_exception.py
@@ -0,0 +1,17 @@
+# coding = utf8
+
+import os
+
+
+class MyException(Exception):
+    """
+
+    """
+    pass
+
+
+if __name__ == '__main__':
+    pass
+
+
+
--- chapter01/demo_code1.py
+++ chapter01/demo_code1.py
@@ -1,25 +1,22 @@
 # coding=utf8
 
-import math
-import random
 
-# 求平方根
-print("square root of 2 is ", math.sqrt(2))
+def demo_python():
+    """
+    >>> import math
 
-# 求1-100的和
-s = sum(range(1, 101))
-print(f"The sum of 1-100 is {s}")
+    # 求平方根
+    >>> print("square root of 2 is ", math.sqrt(2))
+    square root of 2 is  1.4142135623730951
 
-# 求1-12345之间平方数的个数
-c = [k for k in range(12345) if math.sqrt(k) == int(math.sqrt(k))]
-print(f"The count of square number of 0-12345 is {len(c)}, {c[0:5]} ...")
+    # 求1-100的和
+    >>> s = sum(range(1, 101))
+    >>> print(f"The sum of 1-100 is {s}")
+    The sum of 1-100 is 5050
 
-# 求接近的数
-nums = [random.randint(1, 100) for _ in range(20)]
-print(nums)
-result = []
-for n1 in range(len(nums)):
-    for n2 in range(n1+1, len(nums)):
-        if abs(nums[n1]-nums[n2]) < 5:
-            result.append((n1, n2))
-            print((n1, nums[n1]), (n2, nums[n2]))
+    # 求1-12345之间平方数的个数
+    >>> c = [k for k in range(12345) if math.sqrt(k) == int(math.sqrt(k))]
+    >>> print(f"The count of square number of 0-12345 is {len(c)}, [{c[0]}, {c[1]}, {c[2]}, {c[3]}, ...]")
+    The count of square number of 0-12345 is 112, [0, 1, 4, 9, ...]
+    """
+
